e7f7081603f879859a0bb6e595f4068c6bab4496
gt8472 <0123706@kdu-online.com>
2019-11-25 00:59:21 +0800

    warrior， got bug

--- vertical slice/Assets/Scripts/Enemy2.cs
+++ vertical slice/Assets/Scripts/Enemy2.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Enemy2 : MonoBehaviour
 {
+    public Renderer WarriorMaterial;
     public float speed = 10;
     public float hp = 150;//enemy damage
     private float totalHp;
@@ -12,12 +13,18 @@ public class Enemy2 : MonoBehaviour
     private Transform[] positions;
     private int index = 0;
     public int EarnMoney = 10;
+    public float dieTimer = 0.1f;
+    public float continueDieTimer;
+    //public float startDietimer = 0.0f;
+
 
     void Start()
     {
         totalHp = hp;
         positions = Waypoints.positions;
         hpSlider = GetComponentInChildren<Slider>();
+        //JiangShiMaterial.GetComponent<Renderer>().material.SetFloat("_DissolveThreshold", totalHp / hp);
+        //continueDieTimer = startDietimer;
     }
 
     void Update()
@@ -31,6 +38,7 @@ public class Enemy2 : MonoBehaviour
         Vector3 dir = positions[index].position - transform.position;
         float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         transform.LookAt(positions[index].position);
+
     }
 
     void Move()
@@ -44,13 +52,17 @@ public class Enemy2 : MonoBehaviour
         if (index > positions.Length - 1)
         {
             ReachDestination();
+            SoundManager.PlaySound("Takendamage");
         }
-
     }
 
     void ReachDestination()//Link with improve founction 01(EnemySpawner) - When Enemy arrive at end point
     {
         PlayerStats.Lives--;
+
+        //BeenHitTypeB.Instance.HitShake();
+        GameObject.Find("FlashPanel").GetComponent<BeenHitTypeA>().TakeDamage();
+        GameObject.Find("Main Camera").GetComponent<BeenHitTypeB>().HitShake();
         GameObject.Destroy(this.gameObject);
     }
 
@@ -64,6 +76,7 @@ public class Enemy2 : MonoBehaviour
         if (hp <= 0) return;
         hp -= damage;
         hpSlider.value = (float)hp / totalHp;
+        //JiangShiMaterial.SetFloat("_DissolveThreshold", hp / totalHp);
         if (hp <= 0)
         {
             Die();
@@ -72,8 +85,29 @@ public class Enemy2 : MonoBehaviour
     void Die()
     {
         PlayerStats.Money += EarnMoney;
-        GameObject.Destroy(this.gameObject);
+        //transform.Translate(Vector3.up * 90 * DownSpeed * Time.deltaTime);
+        //GameObject.Destroy(this.gameObject);
+        StartCoroutine(WaitDie());
+    }
 
+    IEnumerator WaitDie()
+    {
+        while (true)
+        {
+            continueDieTimer += 1.0f * Time.deltaTime;
+            continueDieTimer = (continueDieTimer > 1.0f) ? 1.0f : continueDieTimer;
+
+            WarriorMaterial.material.SetFloat("_DissolveThreshold1", continueDieTimer);
+
+            if (continueDieTimer >= 1.0f)
+            {
+                GameObject.Destroy(this.gameObject);
+                break;
+            }
+            //yield return new WaitForSecondsRealtime(1);        
+            yield return null;
+        }
     }
 }
+
 
